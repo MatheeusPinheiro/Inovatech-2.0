04d91540464825d398d775bd51c1f6cc12d10d5d
MatheeusPinheiro <matheeuspinheiro0597@gmail.com>
2022-05-21 21:45:15 -0400

    Agora vai

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+/*
+let abrirModal = ()=>  document.querySelector('.modal').style.display = "flex";
+
+let fecharModal = ()=>  document.querySelector('.modal').style.display = "none";
+
+setTimeout(() => {
+   abrirModal();
+}, 5000);
+
+
+let fechar = document.querySelector('.fechar-modal');
+
+fechar.addEventListener('click', ()=>{
+    setTimeout(()=>{
+        fecharModal();
+    }, 1000)
+});
+
+
+*/
